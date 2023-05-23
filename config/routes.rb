a5106caea8bcbb887c67450122c222939f8db01c
efsiqueira <edu.fss14@gmail.com>
2023-05-22 21:47:19 -0300

    Adicionando campo de busca por aluno

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,11 @@ Rails.application.routes.draw do
   resources :responsibles
 
   root to: "projects#index"
-  get '/search', to: 'projects#search'
+  get '/search_project', to: 'projects#search_project', as: :search_project
+  get '/search_student', to: 'students#search_student',  as: :search_student
+
+  get "/sw.js", to: redirect('/404.html')
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
