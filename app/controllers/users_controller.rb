5904304ce0098acdeea00852ff4c2e1f877cab89
efsiqueira <edu.fss14@gmail.com>
2023-06-06 21:01:47 -0300

    Modificando arquivos para se adequar a aplicação

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,70 +1,95 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show edit update destroy ]
+  before_action :set_student, only: %i[ show edit update ]
 
-  # GET /users or /users.json
+  # GET /students or /students.json
   def index
-    @users = User.all
+    @context = "Alunos"
+    @students = User.all.where(status: 1)
   end
 
-  # GET /users/1 or /users/1.json
+  # GET /students/1 or /students/1.json
   def show
+    @projects_by_student = @student.projects.includes(:reports)
   end
 
-  # GET /users/new
+  # GET /students/new
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
+  # GET /students/1/edit
   def edit
   end
 
-  # POST /users or /users.json
+  # POST /students or /students.json
   def create
-    @user = User.new(user_params)
+    @student = User.new(user_params)
+    @student.status = 1
 
     respond_to do |format|
-      if @user.save
-        format.html { redirect_to user_url(@user), notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
+      if @student.save
+        format.html { redirect_to user_url(@student), notice: "Student was successfully created." }
+        format.json { render :show, status: :created, location: @student }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.json { render json: @student.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /users/1 or /users/1.json
+  # PATCH/PUT /students/1 or /students/1.json
   def update
     respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
-        format.json { render :show, status: :ok, location: @user }
+      if @student.update(user_params)
+        format.html { redirect_to user_url(@student), notice: "Student was successfully updated." }
+        format.json { render :show, status: :ok, location: @student }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.json { render json: @student.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /users/1 or /users/1.json
+  # DELETE /students/1 or /students/1.json
   def destroy
-    @user.destroy
+    return "Nenhum aluno foi selecionado." unless params[:selected_ids].present?
+
+    selected_ids = params[:selected_ids]
+    if selected_ids.present?
+      User.where(id: selected_ids).destroy_all
+    else
+      @student.destroy
+    end
 
     respond_to do |format|
-      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
+      format.html { redirect_to users_url, notice: "Aluno excluído com sucesso." }
       format.json { head :no_content }
     end
+  end
+
+  def search_student
+    @name = params[:search]
+    @students_by_name = User.where("LOWER(name) LIKE LOWER(?)", "%#{@name}%")
+      .select(:name, :email, :status)
+      .select("(SELECT COALESCE(SUM(r.reported_effort), 0) 
+        FROM reports r WHERE r.student_id = students.id) AS reported_hours")
+
+    @context = @students_by_name.present? ? "Resultados da busca" : "Nenhum aluno encontrado"
+  end
+
+  def show_inactive_students
+    @context = "Alunos inativos"
+    @inactive_students = User.all.where(status: 0)
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+      @student = User.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name, :email, :password, :status, :organization_id)
+      params.require(:user).permit(:name, :email, :password)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :feedbacks
   resources :classrooms
   resources :students
+  resources :users
   resources :ods_projects
   resources :organizations
   resources :courses
@@ -14,20 +15,24 @@ Rails.application.routes.draw do
   resources :register
 
   root to: "organizations#new"
+
+  # Projetos
   get '/search_project', to: 'projects#search_project', as: :search_project
-  get '/search_student', to: 'students#search_student',  as: :search_student
-  get '/show_inactive_students', to: 'students#show_inactive_students', as: :inactive_students
+
+  # Registro
   get '/register', to: 'responsibles#new'
   post '/register', to: 'responsibles#create'
 
-  get "/sw.js", to: redirect('/404.html')
-
-  resources :students do
+  # Alunos
+  resources :users do
     delete :destroy_selected, on: :collection
   end
-
+  
+  get '/search_student', to: 'users#search_student',  as: :search_student
+  get '/show_inactive_students', to: 'users#show_inactive_students', as: :inactive_students
+  
+  get "/sw.js", to: redirect('/404.html')
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
 end
