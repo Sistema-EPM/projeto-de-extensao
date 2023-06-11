212e6a2c0cb4cc5dc69d34260f477eacae5bd11e
efsiqueira <edu.fss14@gmail.com>
2023-06-11 17:39:38 -0300

    Adicionando atributos do devise nos models

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,3 +1,7 @@
 class Admin < ApplicationRecord
-  belongs_to :organization
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :organization
 end
