08572d9eaa856b81ebe98a1270bf19a484dcc45a
efsiqueira <edu.fss14@gmail.com>
2023-06-13 21:28:51 -0300

    Ajustando migrations

--- db/migrate/20230512235450_add_status_to_student.rb
+++
@@ -1,5 +0,0 @@
-class AddStatusToStudent < ActiveRecord::Migration[7.0]
-  def change
-    add_column :students, :status, :boolean
-  end
-end
--- db/migrate/20230607225834_create_admins.rb
+++ db/migrate/20230607225834_create_admins.rb
@@ -2,8 +2,6 @@ class CreateAdmins < ActiveRecord::Migration[7.0]
   def change
     create_table :admins do |t|
       t.string :name
-      t.string :email
-      t.string :password
       t.references :organization, null: false, foreign_key: true
 
       t.timestamps
