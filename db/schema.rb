ad5f967dbf5fa00b44bf18a71b7dc78e8f258f06
lucasrafaeldesouza <faelsouza.rsg@hotmail.com>
2023-06-14 21:05:21 -0300

    add

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_06_232744) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_14_203731) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "name"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
 
   create_table "archives", force: :cascade do |t|
     t.string "title"
@@ -38,6 +51,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_232744) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_courses_on_user_id"
   end
 
   create_table "feedbacks", force: :cascade do |t|
@@ -61,24 +76,28 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_232744) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "admin_id", null: false
+    t.index ["admin_id"], name: "index_organizations_on_admin_id"
   end
 
   create_table "projects", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name"
     t.string "competency"
     t.string "status"
     t.string "modality"
     t.text "description"
     t.bigint "course_id", null: false
     t.bigint "ods_project_id", null: false
-    t.bigint "feedback_id"
+    t.bigint "feedback_id", null: false
     t.bigint "organization_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["course_id"], name: "index_projects_on_course_id"
     t.index ["feedback_id"], name: "index_projects_on_feedback_id"
     t.index ["ods_project_id"], name: "index_projects_on_ods_project_id"
     t.index ["organization_id"], name: "index_projects_on_organization_id"
+    t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
   create_table "reports", force: :cascade do |t|
@@ -88,7 +107,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_232744) do
     t.bigint "project_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["project_id"], name: "index_reports_on_project_id"
+    t.index ["user_id"], name: "index_reports_on_user_id"
   end
 
   create_table "responsibles", force: :cascade do |t|
@@ -105,25 +126,34 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_232744) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "status"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password"
-    t.boolean "status"
-    t.bigint "organization_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "name"
+    t.boolean "status"
+    t.bigint "organization_id", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.boolean "is_responsible"
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["organization_id"], name: "index_users_on_organization_id"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "archives", "reports"
+  add_foreign_key "courses", "users"
+  add_foreign_key "organizations", "admins"
   add_foreign_key "projects", "courses"
   add_foreign_key "projects", "feedbacks"
   add_foreign_key "projects", "ods_projects"
   add_foreign_key "projects", "organizations"
+  add_foreign_key "projects", "users"
   add_foreign_key "reports", "projects"
+  add_foreign_key "reports", "users"
   add_foreign_key "users", "organizations"
 end
