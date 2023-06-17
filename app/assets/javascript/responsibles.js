a652c0feb9a4e5f2b08e3801cb12fb7f14bd48f9
efsiqueira <edu.fss14@gmail.com>
2023-06-17 00:45:51 -0300

    Corrigindo rotas de responsibles

---
+++ app/assets/javascript/responsibles.js
@@ -0,0 +1,65 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const selectAllCheckbox = document.getElementById('select-all-checkbox');
+  const deleteSelectedBtn = document.getElementById('delete-selected-btn');
+  const editButton = document.getElementById('edit-button');
+  const checkboxes = document.getElementsByClassName('responsible-checkbox');
+
+  selectAllCheckbox.addEventListener('change', function() {
+    const isChecked = this.checked;
+
+    for (let i = 0; i < checkboxes.length; i++) {
+      checkboxes[i].checked = isChecked;
+    }
+
+    deleteSelectedBtn.disabled = !isChecked;
+    editButton.disabled = !isChecked;
+  });
+
+  for (let i = 0; i < checkboxes.length; i++) {
+    checkboxes[i].addEventListener('change', function() {
+      const isAnyCheckboxSelected = Array.from(checkboxes).some(checkbox => checkbox.checked);
+      deleteSelectedBtn.disabled = !isAnyCheckboxSelected;
+      editButton.disabled = !isAnyCheckboxSelected;
+      selectAllCheckbox.checked = isAnyCheckboxSelected && Array.from(checkboxes).every(checkbox => checkbox.checked);
+    });
+  }
+
+  deleteSelectedBtn.addEventListener('click', function() {
+    const selectedIds = Array.from(checkboxes)
+      .filter(checkbox => checkbox.checked)
+      .map(checkbox => checkbox.value);
+
+    if (selectedIds.length > 0) {
+      const confirmDelete = confirm('Tem certeza de que deseja excluir os alunos selecionados?');
+
+      if (confirmDelete) {
+        const deleteForm = document.getElementById('delete-form');
+
+        selectedIds.forEach(function(responsibleId) {
+          const hiddenField = document.createElement('input');
+          hiddenField.setAttribute('type', 'hidden');
+          hiddenField.setAttribute('name', 'selected_ids[]');
+          hiddenField.setAttribute('value', responsibleId);
+          deleteForm.appendChild(hiddenField);
+        });
+
+        deleteForm.submit();
+      }
+    }
+  });
+
+  editButton.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    const selectedIds = Array.from(checkboxes)
+      .filter(checkbox => checkbox.checked)
+      .map(checkbox => checkbox.value);
+
+    if (selectedIds.length === 1) {
+      const responsibleId = selectedIds[0];
+      window.location.href = `/responsibles/${responsibleId}/edit`;
+    } else {
+      alert('Por favor, selecione um único coordenador para editar.');
+    }
+  });
+});
