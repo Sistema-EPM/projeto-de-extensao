ec06fc33c1667c522453c0032b917c107ba7c588
efsiqueira <edu.fss14@gmail.com>
2023-06-22 18:44:10 -0300

    Adicionando configuração de host para todos os ambientes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.hosts << "epm4-edufss14.b4a.run/"
+  config.hosts << "epm4-edufss14.b4a.run"
   config.server.port = 3000
 end
