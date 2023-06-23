bf9bd9d5a9715a1a20fee192c876242f4e3cc558
efsiqueira <edu.fss14@gmail.com>
2023-06-23 11:14:32 -0300

    Ajustando configurações

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.hosts << "epm4-edufss14.b4a.run"
+  # config.hosts << "epm4-edufss14.b4a.run"
 end
